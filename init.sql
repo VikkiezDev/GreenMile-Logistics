2d102d72f8e8896c0c1b4c82c37c02c6a4c3377d
VikkiezDev <vigneshsn23@outlook.com>
2026-09-09 21:01:25 +0530

    Added the init script for DB and schemas

---
+++ init.sql
@@ -0,0 +1,17 @@
+-- 1. Create the database
+CREATE DATABASE greenmile_db;
+GO
+
+-- 2. Switch context to the new database
+USE greenmile_db;
+GO
+
+-- 3. Create schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
